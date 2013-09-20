c75bb1cb8b5593dd5d83b295beb6d3955138e049
Mike Edwards <mikeedwards83@googlemail.com>
2013-09-20 07:56:18 +0100

    stringing together razor and trying to start some search stuff

---
+++ Source/VirtualSummit/Gls/Mapper/Sc/Configuration/Attributes/SitecoreSearch.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VirtualSummit.Gls.Mapper.Sc.Configuration.Attributes
+{
+    public class SitecoreSearch
+    {
+        public string Query { get; set; }
+
+        public SitecoreSearch(string query)
+        {
+            Query = query;
+        }
+    }
+}
